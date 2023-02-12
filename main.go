c867aa06735f1bcd6852936e88eccca82a45d574
ngdangkiet <kietnguyen17052001@gmail.com>
2023-02-12 18:22:19 +0700

    create template project

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+// struct type
+type Person struct {
+	firstName, lastName, email string
+	tickets                    uint
+}
 
+// init global variables
+var conferenceName string = "Golang"
+var conferenceTickets uint = 30
+var remainingTickets uint = 30
+var bookings = make([]Person, 0)
+
+func main() {
+	// get user input
+	// validate user input
+	// book ticket
+	// send ticket
+	// change value remaining ticket
 }
