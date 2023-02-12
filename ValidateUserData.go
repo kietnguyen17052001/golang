40f817865d911c65f3fdbf4172f37af4bab87ca8
ngdangkiet <kietnguyen17052001@gmail.com>
2023-02-12 19:04:59 +0700

    get user input & validate input

--- ValidateUserData.go
+++ ValidateUserData.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"regexp"
+)
+
+func validUserInput(firstName string, lastName string, email string, tickets uint, remainTickets uint) (bool, bool, bool, bool) {
+	return isValidName(firstName), isValidName(lastName), isValidEmail(email), isValidTickets(tickets, remainTickets)
+}
+
+func isValidName(name string) bool {
+	match, _ := regexp.MatchString("[^A-Za-z+]", name)
+	return match
+}
+
+func isValidEmail(email string) bool {
+	match, _ := regexp.MatchString(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`, email)
+	return match
+}
+
+func isValidTickets(tickets uint, remainTickets uint) bool {
+	return tickets > 0 && tickets <= remainTickets
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // struct type
 type Person struct {
 	firstName, lastName, email string
@@ -14,8 +16,28 @@ var bookings = make([]Person, 0)
 
 func main() {
 	// get user input
+	firstName, lastName, email, tickets := getUserInput()
 	// validate user input
+	isValidFirstName, isValidLastName, isValidEmail, isValidTickets := validUserInput(firstName,
+		lastName,
+		email,
+		tickets,
+		remainingTickets)
 	// book ticket
 	// send ticket
 	// change value remaining ticket
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName, lastName, email string
+	var tickets uint
+	fmt.Print("Input firstName:")
+	fmt.Scan(&firstName)
+	fmt.Print("Input lastName:")
+	fmt.Scan(&lastName)
+	fmt.Print("Input email:")
+	fmt.Scan(&email)
+	fmt.Print("Input tickets:")
+	fmt.Scan(&tickets)
+	return firstName, lastName, email, tickets
 }
